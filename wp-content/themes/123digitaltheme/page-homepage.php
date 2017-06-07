6c414089a2cba67619f58727e665fd1a70e415f4
Marty Gribbin <martygribbin11@gmail.com>
2017-06-07 15:00:55 +0100

    test

--- wp-content/themes/123digitaltheme/page-homepage.php
+++ wp-content/themes/123digitaltheme/page-homepage.php
@@ -20,7 +20,7 @@ get_header(); ?>
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class=""></div>
+			<div class="test"></div>
 			<div class=""></div>
 			<div class=""></div>
 			<div class=""></div>
